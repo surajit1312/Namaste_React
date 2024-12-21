34bee03892a9b0644c1dd6cf804906f52894e96f
Surajit Rana <surajit1602@gmail.com>
2024-12-22 00:00:31 +0530

    feat[ep12]: let's build our store

--- P12_Lets_Build_Our_Store/src/store/reducers/cart.reducer.js
+++ P12_Lets_Build_Our_Store/src/store/reducers/cart.reducer.js
@@ -13,7 +13,24 @@ const cartSlice = createSlice({
             state.items.pop();
         },
         clearCart: (state, action) => {
+            /**
+             * With Redux Toolkit, it allows us to mutate current state which
+             * was not allowed in previous versions. 
+             * 
+             * Previous version used to give an error: You cannot Mutate State.
+             * 
+             * But Redux Toolkit, the state which is mutated by developers is 
+             * actually converted to a immutable/new state using ImmerJS.
+             * 
+             * (Immer (German for: always) is a tiny package that allows you to 
+             * work with immutable state in a more convenient way.)
+             */
+
+            // Either mutate state here
             state.items.length = 0;
+            // state.items = [];
+            // Or return a new state as below:
+            // return { items: [] }
         }
     }
 });
